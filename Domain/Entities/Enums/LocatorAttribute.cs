783fc0b7d511938da8a15ccef8384df1fead67de
Owen Burns <owenburns39@gmail.com>
2026-09-20 11:15:49 +0200

    feature: Added the Automation Driver base class

---
+++ Domain/Entities/Enums/LocatorAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Ardalis.SmartEnum;
+
+namespace Domain.Entities.Enums;
+
+public sealed class LocatorAttribute : SmartEnum<LocatorAttribute>
+{
+    public static readonly LocatorAttribute Id = new(nameof(Id), 0, mobileSupported: true);
+    public static readonly LocatorAttribute Name = new(nameof(Name), 1, mobileSupported: false);
+    public static readonly LocatorAttribute Class = new(nameof(Class), 2, mobileSupported: true);
+    public static readonly LocatorAttribute Css = new(nameof(Css), 3, mobileSupported: false);
+    public static readonly LocatorAttribute XPath = new(nameof(XPath), 4, mobileSupported: true);
+    public static readonly LocatorAttribute Text = new(nameof(Text), 5, mobileSupported: false);
+    public static readonly LocatorAttribute AccessibilityId = new(nameof(AccessibilityId), 6, mobileSupported: true);
+
+    public bool MobileSupported { get; }
+
+    private LocatorAttribute(string name, int value, bool mobileSupported)
+        : base(name, value)
+    {
+        MobileSupported = mobileSupported;
+    }
+}
